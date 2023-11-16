2c47db7426ce530a86cb69a93ac96bea865ed14d
Bryce Sexton <brycesexton@Bryces-Laptop.local>
2023-11-15 20:54:12 -0800

    post group work push 11.15

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 // UTILITY FUNCTIONS ===========================================
-const attackBtn = document.getElementById('attack-btn');
-const repairBtn = document.getElementById('repair-btn');
-const defendBtn = document.getElementById('defend-btn');
+const attackBtnEl = document.getElementById('attack-btn');
+const repairBtnEl = document.getElementById('repair-btn');
+const defendBtnEl = document.getElementById('defend-btn');
 
-const playerName = document.getElementById('player-name');
-const playerHealthBar = document.getElementById('player-health-bar');
-const playerHealthNumber = document.getElementById('php');
 
-const enemyName = document.getElementById('enemy-name');
-const enemyHealthBar = document.getElementById('enemy-health-bar');
-const enemyHealthNumber = document.getElementById('ehp');
+const playerNameEl = document.getElementById('player-name');
+const playerHealthBarEl = document.getElementById('player-health-bar');
+const playerHealthNumberEl = document.getElementById('php');
+
+const enemyNameEl = document.getElementById('enemy-name');
+const enemyHealthBarEl = document.getElementById('enemy-health-bar');
+// const playerHealthNumberEl = document.getElementById('ehp');
+const enemyHealthNumberEl = document.getElementById('ehp');
 
 const gameLevel = document.getElementById('level');
+const playLogs = document.getElementById('play-logs');
+
 
 //Generate random number
 function generateRandomNum(num1, num2){
@@ -23,22 +27,26 @@ function generateRandomNum(num1, num2){
 // Create spaceship class - will use for player and enemy
 class Spaceship {
     // pass in name, hp, accuracy, damage, level
-    constructor(name, hp, accuracy, dmg, level) {
+    constructor(name, hpStart, accuracy, dmg, level, hpAdded) {
         this.name = name;
-        this.hp = hp; // 5
+        this.hpStart = hpStart; 
         this.dmg = dmg; 
-        this.accuracy = accuracy; //default: .7
+        this.accuracy = accuracy; 
         this.level = level;
+        this.hpAdded = hpAdded;
+        this.currentHp = hpStart + hpAdded;
+        this.maxHp = hpStart + hpAdded;
     }
         attack(target) {
             setTimeout(() => {
                 let probability = generateRandomNum(10, 1);
                 if (probability > 3) {
-                    target.hp -= this.dmg;
+                    target.currentHp -= this.dmg;
                     //insert some dom manipulation to display the what the console is logging
                     //and depleting enemy hp
                     playLogs.innerHTML = `${this.name} attacked ${target.name} for ${this.dmg} damage!`;
                     console.log(`${this.name} attacked ${target.name} for ${this.dmg} damage!`);
+                    initializeContent(playerOne, enemy1)
                 } else {
                     playLogs.innerHTML = `${this.name} missed!`;
                     console.log(`${this.name} missed!`);
@@ -48,38 +56,55 @@ class Spaceship {
     
         levelUp() {
             this.level++;
-            this.hp = 100;
+            this.currentHp = 100;
             // this.dmg += 0;
             // this.accuracy += 0;
             console.log(`${this.name} leveled up to level ${this.level}!`);
-          }
+        }
+
+        updateHp(damage) {
+            this.maxHp = this.hpStart + this.hpAdded;
+            this.currentHp = this.currentHp - damage;
+        }
 }
 
-// console.log(playerOne.levelUp)
-    //attack event listener
-    
-    
-    attackBtn.addEventListener("click", () => {
-        playerOne.attack();
-        console.log(target.hp);
-    });
+//3 hp values: hp start, hp added, current hp
+//hp start = first number we pull in when game begins (100)
+//hp added = anytime we want to add hp with upgrades or levels increasing
+//current hp = hp start + hp added 
+//method updateCurrentHp that subtracts the dmg from currentHp
+//when level restarts currentHp = hp start + hp added
 
-// Create Player subclass that extends Spaceship
+function initializeContent(player, enemy) {
+    playerNameEl.textContent = player.name;
+    playerHealthBarEl.setAttribute('max', player.maxHp);
+    playerHealthBarEl.setAttribute('value', player.currentHp);
+    playerHealthNumberEl.textContent = player.currentHp;
+
+    enemyNameEl.textContent = enemy.name;
+    enemyHealthBarEl.setAttribute('max', enemy.maxHp);
+    enemyHealthBarEl.setAttribute('value', enemy.currentHp);
+    enemyHealthNumberEl.textContent = enemy.currentHp;
+    //WILL AND BRYCE DEBUGGED THIS SHIT FUCK YALL
+}
+
 class Player extends Spaceship {
-    constructor(name, hp, accuracy, dmg, level) {
-        super(name, hp, accuracy, dmg, level)
+    constructor(name, hpStart, accuracy, dmg, level, hpAdded) {
+        super(name, hpStart, accuracy, dmg, level, hpAdded)
+        this.currentHp = hpStart + hpAdded;
+        this.maxHp = hpStart + hpAdded;
     }
     repair() {
         setTimeout(() => {
-            let health = this.hp;
-            if(health >= 100) {
+            let health = this.currentHp;
+            if(health >= this.hpStart + this.hpAdded) {
                 document.querySelector("#repair-btn").disabled = true;
                 playLogs.innerHTML = "You're at full health!";
                 console.log("You're at full health!");
             } else {
                 //heal 10%-20% of hp
-                this.hp += generateRandomNum(20, 10);
-                playLogs.innerHTML = `${this.name} healed and is now at ${this.hp} HP!`;
+                this.currentHp += generateRandomNum(20, 10);
+                playLogs.innerHTML = `${this.name} healed and is now at ${this.currentHp} HP!`;
                 console.log(`${this.name} healed and is now at ${this.hp} HP!`);
             }
         }, 500);
@@ -93,35 +118,47 @@ class Player extends Spaceship {
                 playLogs.innerHTML = `${this.name} defended the attack!`;
                 console.log(`${this.name} defended the attack!`);
             } else {
-                this.hp -= (damage - defense);
+                this.currentHp -= (damage - defense);
                 playLogs.innerHTML = `${this.name} defended the attack but took ${damage - defense} damage!`;
                 console.log(`${this.name} defended the attack but took ${damage - defense} damage!`);
             }
         }, 500);
     }
 }
-const playerOne = new Player('Player 1', 100, 0.7, 5, 1)
+
+const playerOne = new Player('Player 1', 100, 0.7, 5, 1, 0)
 
 // Create Enemy subclass that extends Spaceship
 class Enemy extends Spaceship {
-    constructor(name, hp, accuracy, dmg, level)  {
-        super(name, hp, accuracy, dmg, level);
-
-        // enemy image
-        // enemy background
-        
+    constructor(name, hpStart, accuracy, dmg, level, hpAdded)  {
+        super(name, hpStart, accuracy, dmg, level, hpAdded);
+        this.currentHp = hpStart + hpAdded;
+        this.maxHp = hpStart + hpAdded;
     }
 }
-const enemy1 = new Enemy("Starshredder", 100, 0, 0, 1);
-const enemy2 = new Enemy("Nebula Scourge", 100, 0, 0, 2);
-const enemy3 = new Enemy("Cosmic Menace", 100, 0, 0, 3);
-const enemy4 = new Enemy("Galactic Destructor", 100, 0, 0, 4);
-const enemy5 = new Enemy("Dark Matter Raider", 100, 0, 0, 5);
-const enemy6 = new Enemy("Quantum Marauder", 100, 0, 0, 6);
-const enemy7 = new Enemy("Interstellar Annihilator", 100, 0, 0, 7);
-const enemy8 = new Enemy("Plasma Leviathan", 100, 0, 0, 8);
-const enemy9 = new Enemy("GARBAGE MAN", 100, 0, 0, 9);
-const enemy10 = new Enemy("Arthur SS", 100, 0, 0, 10);
+
+const enemy1 = new Enemy("Starshredder", 100, 0, 0, 1, 0);
+const enemy2 = new Enemy("Nebula Scourge", 100, 0, 0, 2, 0);
+const enemy3 = new Enemy("Cosmic Menace", 100, 0, 0, 3, 0);
+const enemy4 = new Enemy("Galactic Destructor", 100, 0, 0, 4, 0);
+const enemy5 = new Enemy("Dark Matter Raider", 100, 0, 0, 5, 0);
+const enemy6 = new Enemy("Quantum Marauder", 100, 0, 0, 6, 0);
+const enemy7 = new Enemy("Interstellar Annihilator", 100, 0, 0, 7, 0);
+const enemy8 = new Enemy("Plasma Leviathan", 100, 0, 0, 8, 0);
+const enemy9 = new Enemy("GARBAGE MAN", 100, 0, 0, 9, 0);
+const enemy10 = new Enemy("Arthur SS", 100, 0, 0, 10, 0);
+
+
+
+// const playerNameEl = document.getElementById('player-name');
+// const playerHealthBarEl = document.getElementById('player-health-bar');
+// const playerHealthNumberEl = document.getElementById('php');
+
+// const enemyNameEl = document.getElementById('enemy-name');
+// const enemyHealthBarEl = document.getElementById('enemy-health-bar');
+// const enemyHealthNumberEl = document.getElementById('ehp');
+
+// const gameLevel = document.getElementById('level');
 
 // Show docking modal
 // GOING TO NEED TO WORK WITH "HIDDEN SCREENS" FOR THIS
@@ -139,7 +176,9 @@ const enemy10 = new Enemy("Arthur SS", 100, 0, 0, 10);
 // });
 
 // START LEVEL 1 ===========================================
-// const levelScreen;
+
+initializeContent(playerOne, enemy1);
+// FIX PLAYER ONE NAME
 
 
 // Grab all needed HTML Elements
@@ -157,18 +196,23 @@ const enemy10 = new Enemy("Arthur SS", 100, 0, 0, 10);
 
 
 //attack event listener    
- attackBtn.addEventListener("click", () => {
+ attackBtnEl.addEventListener("click", () => {
     playerOne.attack(enemy1);
     console.log(enemy1.hp);
 });
     
 //repair event listener
-repairBtn.addEventListener("click", () => {
+repairBtnEl.addEventListener("click", () => {
     playerOne.repair();
     console.log(playerOne.hp);
 });
 //defend event listener
-defendBtn.addEventListener("click", () => {
+defendBtnEl.addEventListener("click", () => {
     playerOne.defend(enemy1);
     console.log(playerOne.hp);
 });
+
+
+
+//CODE FOR TURNS
+//on attack set interval for rebutl
